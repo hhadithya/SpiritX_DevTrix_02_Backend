2dc3168916f3eaa443f4ab0da1198b6f99c1a92a
nethmalgunawardhana <nethmalgunawardhana@gmail.com>
2025-03-09 13:38:51 +0530

    feat(players): add route for single player creation

--- src/routes/playerRoutes.ts
+++ src/routes/playerRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createPlayers} from '../database/playersupdate';
+import {createPlayers,createPlayer} from '../database/playersupdate';
 
 const playerrouter = express.Router();
 
 // Player routes
 playerrouter.post('/create', createPlayers);
+playerrouter.post('/create/single', createPlayer);
 
 
 export default playerrouter;
