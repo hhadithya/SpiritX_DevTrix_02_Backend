ed88fb458d7a0f3899e3dcc240d2f3567238b9fa
Sakindu Ransindu <sakindu.ransindu@gmail.com>
2025-03-09 12:52:05 +0530

    refactor: createTeams

--- src/controllers/teamsController.ts
+++ src/controllers/teamsController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { firestore } from '../firebase/firebase';
+import {  FieldValue } from 'firebase-admin/firestore';
+
 
 export const getTeam = async (req: Request, res: Response): Promise<any> => {
     try {
@@ -29,31 +31,13 @@ export const getTeam = async (req: Request, res: Response): Promise<any> => {
     }
 };
 
-export const getPlayerById = async (req: Request, res: Response): Promise<any> => {
-    try {
-        const playerRef = firestore.collection('players').doc(req.params.id);
-        const playerDoc = await playerRef.get();
-
-        if (!playerDoc.exists || !playerDoc.data()?.activeStatus) {
-            return res.status(404).json({ error: 'Player not found' });
-        }
-
-        res.json({ id: playerDoc.id, ...playerDoc.data() });
-    } catch (error) {
-        console.error('Error fetching player details:', error);
-        res.status(500).json({ error: 'Failed to fetch player details' });
-    }
-};
-
 export const createTeam = async (req: Request, res: Response): Promise<any> => {
-    const { tournamentId, teamName, players, budgetRemaining } = req.body;
+    const { teamName, players , userId } = req.body;
 
-    if (!tournamentId || !teamName || !players) {
+    console.log(`tring to create team ${teamName} for user ${userId}`);
+
+    if (!teamName || !userId) {
         return res.status(400).json({ error: 'Missing required fields' });
-    }
-
-    if (typeof tournamentId !== 'string' || tournamentId.length < 1 || tournamentId.length > 50) {
-        return res.status(400).json({ error: 'Invalid tournamentId value' });
     }
 
     if (typeof teamName !== 'string' || teamName.length < 1 || teamName.length > 30) {
@@ -64,17 +48,36 @@ export const createTeam = async (req: Request, res: Response): Promise<any> => {
         return res.status(400).json({ error: 'Invalid players value' });
     }
 
-    const tournamentRef = firestore.collection('tournaments').where("tournamentId","==",tournamentId);
-    const tournamentDoc = await tournamentRef.get();
+    console.log(`team ${teamName} creation basic validation completed`);
 
-    if (tournamentDoc.empty) {
-        return res.status(404).json({ error: 'Tournament not found' });
+    const userRef = firestore.collection('users').doc(userId);
+    const userDoc = await userRef.get();
+
+    console.log(`userId ${userId} is exist: ${userDoc.exists} userdoc: ${userDoc.data()}`);
+    if (!userDoc.exists) {
+        return res.status(404).json({ error: 'user not found' });
     }
 
-    try {
-        const newTeam = { tournamentId, teamName, players, budgetRemaining, };
-        const teamRef = await firestore.collection('userTeams').add(newTeam);
+    const userTeams = userDoc.data()?.teams as string[] || [];
+    const userTeamsCreated = userDoc.data()?.teamsCreated as number || 0;
 
+    console.log(`user teams ${userTeams} before adding new team`);
+    if(userTeams.find((team: string) => team === teamName)){
+        return res.status(400).json({ error: 'Team already created' });
+    }
+
+    const timestamp = new Date();
+
+    try {
+        const newTeam = { teamName, players, createdAt: timestamp};
+        const teamRef = await firestore.collection('userTeams');
+        const snapshot = await teamRef.where('teamName', '==', teamName).get();
+        if (!snapshot.empty) {
+            return res.status(400).json({ error: 'Team already exists' });
+        }
+        const newTeamDocName = userId +"-"+ userTeamsCreated.toString();
+        await teamRef.doc(newTeamDocName).set(newTeam);
+        await userRef.update({ teams: [...userTeams, newTeamDocName], teamsCreated: FieldValue.increment(1) });
         res.status(201).json({ id: teamRef.id, ...newTeam });
     } catch (error) {
         console.error('Error adding team:', error);
@@ -82,60 +85,3 @@ export const createTeam = async (req: Request, res: Response): Promise<any> => {
     }
 };
 
-export const updatePlayer = async (req: Request, res: Response): Promise<any> => {
-    try {
-        const { name, category, basePrice, activeStatus } = req.body;
-        const playerRef = firestore.collection('players').doc(req.params.id);
-
-        const playerDoc = await playerRef.get();
-        if (!playerDoc.exists || !playerDoc.data()?.activeStatus) {
-            return res.status(404).json({ error: 'Player not found' });
-        }
-
-        const updates: any = {};
-        if (name && typeof name === 'string' && name.length >= 1 && name.length <= 50) updates.name = name;
-        if (category && typeof category === 'string' && category.length >= 1 && category.length <= 30) updates.category = category;
-
-
-        if (basePrice && typeof basePrice === 'number' && basePrice > 0) updates.basePrice = basePrice;
-        if (basePrice) {
-            const parsedBasePrice = parseFloat(basePrice);
-            if (!isNaN(parsedBasePrice) && parsedBasePrice > 0) {
-            updates.basePrice = parsedBasePrice;
-            }
-        }   
-        
-        if (typeof activeStatus === 'boolean') updates.activeStatus = activeStatus;
-
-        if (Object.keys(updates).length === 0) {
-            return res.status(400).json({ error: 'No valid fields provided for update' });
-        }
-
-        await playerRef.update(updates);
-        res.json({ id: req.params.id, ...updates });  // returns the fields that were updated
-    } catch (error) {
-        console.error('Error updating player:', error);
-        res.status(500).json({ error: 'Failed to update player' });
-    }
-};
-
-
-export const deletePlayer = async (req: Request, res: Response): Promise<any> => {
-    try {
-        const playerRef = firestore.collection('players').doc(req.params.id);
-        const playerDoc = await playerRef.get();
-
-        if (!playerDoc.exists || !playerDoc.data()?.activeStatus) {
-            return res.status(404).json({ error: 'Player not found' });
-        }
-
-        // await playerRef.delete();  // THIS IS PERMANENT DELETE
-
-        // instead, we update the activeStatus field to false
-        await playerRef.update({ activeStatus: false });
-        res.json({ message: 'Player deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting player:', error);
-        res.status(500).json({ error: 'Failed to delete player' });
-    }
-}
